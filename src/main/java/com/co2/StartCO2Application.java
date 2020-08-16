c8b43299162cb4ba907ca985f86524a45ca7c461
GreeshmaRemanan <67099620+GreeshmaRemanan@users.noreply.github.com>
2020-08-17 01:58:42 +0530

    added data for weeklyreport

--- src/main/java/com/co2/StartCO2Application.java
+++ src/main/java/com/co2/StartCO2Application.java
@@ -1,5 +1,7 @@
 package com.co2;
 
+import java.sql.Date;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -57,7 +59,19 @@ public class StartCO2Application {
 			sensor1 = new Sensor(2L, "CO2Sensor2", "CO2S2", district1);
 			sensorRepository.save(sensor1);
 			
+			Date date = new Date(System.currentTimeMillis());
 			co2ReadingRepository.save(new CO2Reading(1L, client, district1, city1,sensor1, 1.02, new java.sql.Date(System.currentTimeMillis())));
+			co2ReadingRepository.save(new CO2Reading(1L, client, district1, city1,sensor1, 1.02, new java.sql.Date(date.getYear(),date.getMonth(),date.getDay()-1)));
+			co2ReadingRepository.save(new CO2Reading(3L, client, district1, city1,sensor1, 1.03, new java.sql.Date(date.getYear(),date.getMonth(),date.getDay()-2)));
+			co2ReadingRepository.save(new CO2Reading(4L, client, district1, city1,sensor1, 0.28, new java.sql.Date(date.getYear(),date.getMonth(),date.getDay()-3)));
+			co2ReadingRepository.save(new CO2Reading(5L, client, district1, city1,sensor1, 1.02, new java.sql.Date(date.getYear(),date.getMonth(),date.getDay()-4)));
+			co2ReadingRepository.save(new CO2Reading(6L, client, district1, city1,sensor1, 1.01, new java.sql.Date(date.getYear(),date.getMonth(),date.getDay()-5)));
+			co2ReadingRepository.save(new CO2Reading(7L, client, district1, city1,sensor1, 1.02, new java.sql.Date(date.getYear(),date.getMonth(),date.getDay()-6)));
+			co2ReadingRepository.save(new CO2Reading(8L, client, district2, city1,sensor1, 1.05, new java.sql.Date(System.currentTimeMillis())));
+			co2ReadingRepository.save(new CO2Reading(9L, client, district2, city1,sensor1, 1.02, new java.sql.Date(date.getYear(),date.getMonth(),date.getDay()-1)));
+			co2ReadingRepository.save(new CO2Reading(10L, client, district2, city1,sensor1, 0.89, new java.sql.Date(date.getYear(),date.getMonth(),date.getDay()-2)));
+			
+			
 		};
 	}
 }
